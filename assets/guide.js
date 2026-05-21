385f4824b713ae0502be09d2a9b6d7237caaa263
joseph <150156332+Joseph97CT@users.noreply.github.com>
2026-05-21 15:41:51 +0200

    clean repo

--- assets/guide.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  "use strict";
-
-  const isGuide = (window.location.pathname || "").includes("/guides/");
-  if (!isGuide) return;
-
-  document.documentElement.classList.add("is-guide", "guide-stable-mode");
-  document.body.classList.add("page-guide");
-})();
